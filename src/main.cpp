80a520b5611a5d1a2ab90996ba18a385a1bb5a31
Ofenkaes <126564621+Ofenkaes@users.noreply.github.com>
2023-04-04 16:24:05 +0200

    Wild

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 ﻿#include <cstdlib>
 #include <algorithm>
+#include <iostream>
 
 #include "raylib.h"
-
+#include "raymath.h"
 #include "config.h"
 
 int main() {
@@ -10,7 +11,7 @@ int main() {
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
-    SetTargetFPS(60);
+    SetTargetFPS(200);
 
 #ifdef GAME_START_FULLSCREEN
     ToggleFullscreen();
@@ -24,30 +25,63 @@ int main() {
     float renderScale{}; //those two are relevant to drawing and code-cleanliness
     Rectangle renderRec{};
 
+    int dudeX = 10;
+    int dudeY = 100;
+
+    Vector2 dudePos{10, 100};
+    Vector2 dudeVel{};
+    float maxSpeed = 2;
+
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         if (IsKeyDown(KEY_LEFT_ALT) && IsKeyPressed(KEY_ENTER)) { //Fullscreen logic.
             if (IsWindowFullscreen()) {
                 ToggleFullscreen();
-                SetWindowSize(Game::ScreenWidth,Game::ScreenHeight);
+                SetWindowSize(Game::ScreenWidth, Game::ScreenHeight);
             } else {
                 SetWindowSize(GetMonitorWidth(GetCurrentMonitor()), GetMonitorHeight(GetCurrentMonitor()));
                 ToggleFullscreen();
             }
         }
-        // Updates that are made by frame are coded here
-        // ...
-        // ...
+
+        dudeVel = Vector2Scale(dudeVel, 0.95);
+
+        if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            maxSpeed = 6;
+        } else {
+            maxSpeed = 2;
+        }
+
+        if (IsKeyDown(KEY_D)) {
+            dudeVel.x += maxSpeed;
+        }
+
+        if (IsKeyDown(KEY_A)) {
+            dudeVel.x -= maxSpeed;
+        }
+
+        if (IsKeyDown(KEY_W)) {
+            dudeVel.y -= maxSpeed;
+        }
+
+        if (IsKeyDown(KEY_S)) {
+            dudeVel.y += maxSpeed;
+        }
+
+        if (Vector2Length(dudeVel) > maxSpeed)
+            dudeVel = Vector2Scale(Vector2Normalize(dudeVel), maxSpeed);
+
+        dudePos = Vector2Add(dudePos, dudeVel);
 
         BeginDrawing();
         // You can draw on the screen between BeginDrawing() and EndDrawing()
         // For the letterbox we draw on canvas instad
         BeginTextureMode(canvas);
         { //Within this block is where we draw our app to the canvas.
-            ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
+            ClearBackground(BLUE);
+            DrawText(TextFormat("Current FPS: %i",GetFPS()), 10, 10, 30, BLACK);
+            DrawTexture(myTexture, dudePos.x, dudePos.y, BLUE);
         }
         EndTextureMode();
         //The following lines put the canvas in the middle of the window and have the negative as black
