04d67b5557583d672f5b83fa3f2103f52b215835
Tamina <11018565@stud.hochschule-heidelberg.de>
2023-07-02 15:12:47 +0200

    added level 1.1 titled data

---
+++ assets/maps/Tileset_old_blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Tileset_old_blocks" tilewidth="24" tileheight="24" tilecount="35" columns="7">
+ <image source="../../Grafiken/Vorlagen/Tileset_New.png" width="168" height="120"/>
+</tileset>
