b3357728c3a1048ef461542fe9157ed189274fca
KoMeyer <konstantinmeyer0@gmail.com>
2023-08-07 18:50:33 +0200

    Revert "done some enemy stuff"
    This reverts commit 857c0ae2ded1a6b32de6c2487e642ef474378f70.

--- src/GameObjects/Enemy/Enemy.h
+++ src/GameObjects/Enemy/Enemy.h
@@ -9,22 +9,5 @@ namespace Game {
         virtual void updateEnemy() = 0;
         virtual void moveAnimation() = 0;
         virtual void idleAnimation() = 0;
-        virtual void huntPlayer() = 0;
-        virtual void drawEnemy() = 0;
-        int moveSpeed;
-        bool destructable;
-        int direction = 0;
-        bool playerDetected = false;
-        int framesCounter = 0;
-        int framesSpeed = 1;
-        int currentFrame = 0;
-
-        enum direction {
-            idle,
-            moveUp,
-            moveLeft,
-            moveDown,
-            moveRight
-        };
     };
 }
--- src/GameObjects/Enemy/ImmortalEnemy.cpp
+++ src/GameObjects/Enemy/ImmortalEnemy.cpp
@@ -5,118 +5,21 @@
 #include "ImmortalEnemy.h"
 
 game::ImmortalEnemy::ImmortalEnemy(int posX, int posY) {
-    this->setPos((float) posX, (float) posY);
-    this->setTexture(texIdle);
-    this->setCollRec((float) posX, (float) posY, 24, 24);
-    this->moveSpeed = 1;
-    this->destructable = false;
+
 }
 
 void game::ImmortalEnemy::move() {
-    switch (direction) {
-        case moveUp:
-            this->pos.y -= (float) moveSpeed;
-            break;
-        case moveLeft:
-            this->pos.x -= (float) moveSpeed;
-            break;
-        case moveDown:
-            this->pos.y += (float) moveSpeed;
-            break;
-        case moveRight:
-            this->pos.x += (float) moveSpeed;
-            break;
-    }
+
 }
 
 void game::ImmortalEnemy::updateEnemy() {
-    this->setCollRec(this->getPos().x, this->getPos().y, 24, 24);
+
 }
 
 void game::ImmortalEnemy::moveAnimation() {
-    framesCounter++;
-    switch (direction) {
-        case moveUp:
-            if (framesCounter >= (13 / framesSpeed)) {
 
-                framesCounter = 0;
-                currentFrame++;
-
-                if (currentFrame > 8) currentFrame = 0;
-
-                recUp.x = (float) currentFrame * (float) texUp.width / 9;
-            }
-            break;
-        case moveLeft:
-            if (framesCounter >= (13 / framesSpeed)) {
-
-                framesCounter = 0;
-                currentFrame++;
-
-                if (currentFrame > 8) currentFrame = 0;
-
-                recLeft.x = (float) currentFrame * (float) texLeft.width / 9;
-            }
-            break;
-        case moveDown:
-            if (framesCounter >= (13 / framesSpeed)) {
-
-                framesCounter = 0;
-                currentFrame++;
-
-                if (currentFrame > 8) currentFrame = 0;
-
-                recDown.x = (float) currentFrame * (float) texDown.width / 9;
-            }
-            break;
-        case moveRight:
-            if (framesCounter >= (13 / framesSpeed)) {
-
-                framesCounter = 0;
-                currentFrame++;
-
-                if (currentFrame > 8) currentFrame = 0;
-
-                recRight.x = (float) currentFrame * (float) texRight.width / 9;
-            }
-            break;
-    }
 }
 
 void game::ImmortalEnemy::idleAnimation() {
-    framesCounter++;
-    if (framesCounter >= (13 / framesSpeed)) {
 
-        framesCounter = 0;
-        currentFrame++;
-
-        if (currentFrame > 8) currentFrame = 0;
-
-        recIdle.x = (float) currentFrame * (float) texIdle.width / 9;
-    }
-}
-
-void game::ImmortalEnemy::huntPlayer() {
-    // no code yet so for now
-    move();
-}
-
-void game::ImmortalEnemy::drawEnemy() {
-    switch (direction) {
-        case idle:
-            DrawTexturePro(texIdle, recIdle, this->getCollRec(), {}, 0, WHITE);
-            break;
-        case moveUp:
-            DrawTexturePro(texUp, recUp, this->getCollRec(), {}, 0, WHITE);
-            break;
-        case moveLeft:
-            DrawTexturePro(texLeft, recLeft, this->getCollRec(), {}, 0, WHITE);
-            break;
-        case moveDown:
-            DrawTexturePro(texDown, recDown, this->getCollRec(), {}, 0, WHITE);
-            break;
-        case moveRight:
-            DrawTexturePro(texRight, recRight, this->getCollRec(), {}, 0, WHITE);
-            break;
-    }
 }
