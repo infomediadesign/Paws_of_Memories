0d269d4ac3888693a705b8e7e299ac567d652142
KoMeyer <konstantinmeyer0@gmail.com>
2023-08-07 16:47:34 +0200

    pls build

--- src/main.cpp
+++ src/main.cpp
@@ -41,8 +41,6 @@ int main() {
                 ToggleFullscreen();
             }
         }
-        Game::currentScreen->setRScale(renderScale);
-        Game::currentScreen->Update();
 
         BeginDrawing();
         // You can draw on the screen between BeginDrawing() and EndDrawing()
@@ -68,6 +66,8 @@ int main() {
                        {}, 0, WHITE);
 
         EndDrawing();
+        Game::currentScreen->setRScale(renderScale);
+        Game::currentScreen->Update();
     } // Main game loop end
 
     // De-initialization here
