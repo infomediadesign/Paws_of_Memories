f21f19abbbff677da9f11afa0e57e9bfe4e5d3d5
KoMeyer <konstantinmeyer0@gmail.com>
2023-08-07 19:09:59 +0200

    Added another FOlder and 1 class

---
+++ src/GameObjects/Enemies/Enemy.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../../Sprite.h"
+
+namespace Game {
+    class Enemy : public Sprite {
+    public:
+        virtual void move() = 0;
+        virtual void updateEnemy() = 0;
+        virtual void moveAnimation() = 0;
+        virtual void idleAnimation() = 0;
+        virtual void huntPlayer() = 0;
+        virtual void drawEnemy() = 0;
+        int moveSpeed = 1;
+        bool destructable = false;
+        int direction = 0;
+        bool playerDetected = false;
+        int framesCounter = 0;
+        int framesSpeed = 1;
+        int currentFrame = 0;
+
+        enum direction {
+            idle,
+            moveUp,
+            moveLeft,
+            moveDown,
+            moveRight
+        };
+    };
+}
