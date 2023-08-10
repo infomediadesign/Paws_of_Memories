ce446517458592d27fa508c2c9b63193d14cad31
Nicole Schaffner <11018880@STUD.HOCHSCHULE-HEIDELBERG.DE>
2023-08-10 09:29:59 +0200

    Fixed cat animation

--- src/GameScreen.cpp
+++ src/GameScreen.cpp
@@ -1490,9 +1490,7 @@ void Game::GameScreen::drawStartScreen() {
 }
 
 void Game::GameScreen::drawGameOver() {
-    DrawTexturePro(background, backgroundFrame,
-                   Rectangle{0, 0, backgroundFrame.width, backgroundFrame.height},
-                   {}, 0, WHITE);
+
     framesCounter++;
     if (framesCounter >= (60 / framesSpeed)) {
 
@@ -1509,9 +1507,7 @@ void Game::GameScreen::drawGameOver() {
         backgroundFrame.x = (float) currentFrame * (float) background.width / 11;
     }
 
-    DrawTexturePro(GameOver, GameOverFrame,
-                   Rectangle{0, 0, GameOverFrame.width, GameOverFrame.height},
-                   {}, 0, WHITE);
+
     framesCounter++;
     if (framesCounter >= (60 / framesSpeed)) {
 
@@ -1526,6 +1522,12 @@ void Game::GameScreen::drawGameOver() {
 
         GameOverFrame.x = (float) currentFrame * (float) GameOver.width / 2;
     }
+    DrawTexturePro(background, backgroundFrame,
+                   Rectangle{0, 0, backgroundFrame.width, backgroundFrame.height},
+                   {}, 0, WHITE);
+    DrawTexturePro(GameOver, GameOverFrame,
+                   Rectangle{0, 0, GameOverFrame.width, GameOverFrame.height},
+                   {}, 0, WHITE);
 }
 
 void Game::GameScreen::drawGallery() {
