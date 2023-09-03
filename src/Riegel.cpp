a152639baf5ef38e07e1e9ec3c6863631b7fc1ce
Tamina <11018565@stud.hochschule-heidelberg.de>
2023-09-03 15:10:05 +0200

    ?

---
+++ src/Riegel.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Riegel.h"
+
+
+Game::Riegel::Riegel(int RiegelX, int RiegelY) {
+    this->setPos(RiegelX, RiegelY);
+
+}
+
+void Game::Riegel::ColUpdate() {
+    this->collRectangle = {this->getPos().x, this->getPos().y, 24, 24};
+}
+
+void Game::Riegel::PlaceUpdate() {
+
+}
+
+void Game::Riegel::move() {
+    setCollRec(this->getPos().x, this->getPos().y, 24, 24);
+
+
+    // Eingabe abfragen
+    Vector2 mousePosition = GetMousePosition();
+
+    mousePosition.x /= renderScale;
+    mousePosition.y /= renderScale;
+
+
+    //std::cout << mousePosition.x << mousePosition.y << std::endl;
+    if (riegelCanMove) {
+        if (CheckCollisionPointRec(mousePosition, {pos.x, pos.y, 24, 24}) &&
+            IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            this->pos.x = mousePosition.x - 24 / 2;
+        }
+    }
+}
+
+
+
+
+
